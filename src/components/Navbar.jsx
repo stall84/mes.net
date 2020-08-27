b65a8ba17b4555ec019713f2bce7202819cdd6c7
Michael Stallings <stall84@gmail.com>
2020-08-27 09:57:49 -0400

    Added Devicons to Resume section. Updated Portfolio to link to React-Native port of Beach-Me

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,159 +1,145 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
-        AppBar,
-        Toolbar,
-        ListItem,
-        IconButton,
-        ListItemText,
-        ListItemIcon,
-        Avatar,
-        Drawer,
-        Divider,
-        List,
-        Typography,
-        Box
-        } from '@material-ui/core';
+  AppBar,
+  Toolbar,
+  ListItem,
+  IconButton,
+  ListItemText,
+  ListItemIcon,
+  Avatar,
+  Drawer,
+  Divider,
+  List,
+  Typography,
+  Box,
+} from "@material-ui/core";
 import {
-        FlightTakeoff,
-        AssignmentInd,
-        Home,
-        Apps,
-        ContactMail
-        } from '@material-ui/icons';
-import {
-        makeStyles
-        } from '@material-ui/styles';
-import Uranus from '../images/uranus3.png';
-
+  FlightTakeoff,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
+import Uranus from "../images/uranus3.png";
 
 // Define Styles using Material UI's makeStyles hook
 
 const useStyles = makeStyles({
-    root: {
-        backgroundColor: '#222',
-        color: '#5dbcd2'
-    },
-    menuItems: { 
-        color: '#ff6347'
-    },
-    hamburger: {
-        color: '#ff6347'
-    },
-    headerText: {
-        color: '#ffffff'
-    },
-    menuSlider: {
-        width: '18vw',
-        [`@media (max-width: 600px)`]: {width: '40vw'},
-        height: '100vh',
-        backgroundColor: '#444444'
-    },
-    avatar: {
-        margin: '1rem auto',
-        width: '5rem',
-        height: '5rem'
-    }
+  root: {
+    backgroundColor: "#222",
+    color: "#5dbcd2",
+  },
+  menuItems: {
+    color: "#ff6347",
+  },
+  hamburger: {
+    color: "#ff6347",
+  },
+  headerText: {
+    color: "#ffffff",
+  },
+  menuSlider: {
+    width: "18vw",
+    [`@media (max-width: 600px)`]: { width: "40vw" },
+    height: "100vh",
+    backgroundColor: "#444444",
+  },
+  avatar: {
+    margin: "1rem auto",
+    width: "5rem",
+    height: "5rem",
+  },
 });
 
-// Array of menu icons 
+// Array of menu icons
 
 const menuItems = [
-    {
-        listIcon: <Home/>,
-        listText: 'Home',
-        listPath: '/'
-        
-    },
-    {
-        listIcon: <AssignmentInd/>,
-        listText: 'Resume',
-        listPath: '/resume'
-        
-    },
-    {
-        listIcon: <Apps/>,
-        listText: 'Portfolio',
-        listPath: '/portfolio'
-        
-        
-    },
-    {
-        listIcon: <ContactMail/>,
-        listText: 'Contact',
-        listPath: '/contact'
-       
-    }
-]
-
+  {
+    listIcon: <Home />,
+    listText: "Home",
+    listPath: "/",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+    listPath: "/resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+    listPath: "/portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contact",
+    listPath: "/contact",
+  },
+];
 
 export default function Navbar() {
+  const classes = useStyles();
 
-    
-    const classes = useStyles();
+  const [state, setState] = useState({
+    right: false,
+  });
 
-    const [state, setState] = useState({
-        right: false
-    })
+  const toggleSlider = (slider, open) => () => {
+    setState({
+      ...state,
+      [slider]: open,
+    });
+  };
 
-    const toggleSlider = (slider, open) => () => {
-        setState({
-            ...state, 
-            [slider]: open
-        })
-    }
-
-    const SideList = slider => (
-
-        <Box 
-            className={classes.menuSlider} 
-            component="div" 
-            onClick={toggleSlider(slider, false)}
+  const SideList = (slider) => (
+    <Box
+      className={classes.menuSlider}
+      component="div"
+      onClick={toggleSlider(slider, false)}
+    >
+      <Avatar className={classes.avatar} src={Uranus} alt="Uranus" />
+      <Divider />
+      <List>
+        {/* Looping over list item icons array we defined above function */}
+        {menuItems.map((listItem, key) => (
+          <ListItem
+            className={classes.menuItems}
+            button
+            key={key}
+            component={Link}
+            to={listItem.listPath}
+          >
+            <ListItemIcon className={classes.menuItems}>
+              {listItem.listIcon}
+            </ListItemIcon>
+            <ListItemText>{listItem.listText}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <React.Fragment>
+      <Box component="nav">
+        <AppBar position="static" className={classes.root}>
+          <Toolbar>
+            <IconButton onClick={toggleSlider("right", true)}>
+              <FlightTakeoff className={classes.hamburger} />
+            </IconButton>
+            <Typography className={classes.headerText} variant="h5">
+              Menu
+            </Typography>
+            <Drawer
+              anchor="right"
+              open={state.right}
+              onClose={toggleSlider("right", false)}
             >
-            <Avatar className={classes.avatar} src={Uranus} alt="Uranus" />
-                <Divider />
-                    <List>
-                        {/* Looping over list item icons array we defined above function */}
-                        {menuItems.map((listItem, key) => (
-                        <ListItem className={classes.menuItems} button key={key} component={Link} to={listItem.listPath} >
-                            <ListItemIcon className={classes.menuItems} >
-                                {listItem.listIcon}
-                            </ListItemIcon>
-                            <ListItemText >
-                                {listItem.listText}
-                            </ListItemText>
-                        </ListItem>
-                        ))}
-                    </List>
-        </Box>
-    )
-    return (
-        <React.Fragment>
-
-        
-
-        <Box component="nav" >
-
-            <AppBar position="static" className={classes.root} >
-                <Toolbar>
-                    <IconButton onClick={toggleSlider('right', true)}>
-                        <FlightTakeoff className={classes.hamburger} />
-                    </IconButton>
-                    <Typography className={classes.headerText} variant="h5">
-                    Personal Site - Portfolio
-                    </Typography>
-                    <Drawer
-                        anchor='right'
-                        open={state.right}
-                        onClose={toggleSlider('right', false)}
-                    >
-                        {SideList('right')}
-                    </Drawer>
-                </Toolbar>
-            </AppBar>
-
-        </Box>
-
-        </React.Fragment>
-    )
+              {SideList("right")}
+            </Drawer>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </React.Fragment>
+  );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,75 +1,166 @@
-import React from 'react';
-import Navbar from './Navbar';
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core/';
-import PDF from '../docs/MichaelStallingsResume.pdf';
-import DevIcon from 'devicon-react-svg';
-
+import React from "react";
+import Navbar from "./Navbar";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core/";
+import PDF from "../docs/MichaelStallingsResume.pdf";
+import DevIcon from "devicon-react-svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    textAlign: 'center',
-    marginTop: '20vh'
+    textAlign: "center",
+    marginTop: "20vh",
   },
   gridHolder: {
-    width: '100%',
+    width: "100%",
   },
   a: {
-    textDecoration: 'none'
+    textDecoration: "none",
   },
   button: {
-    textDecoration: 'none',
-    backgroundColor: 'tomato',
-    color: 'white'
+    textDecoration: "none",
+    backgroundColor: "tomato",
+    color: "white",
   },
   techHolder: {
-    marginTop: '100px'
-  }
-  
-  
+    marginTop: "100px",
+  },
 }));
 
 const devIconStyle = {
-  fill: 'white',
-  width: '85px'
-}
+  fill: "white",
+  width: "85px",
+};
 
 export default function Resume() {
   const classes = useStyles();
 
-  
   return (
-  
     <React.Fragment>
-    <Navbar />
+      <Navbar />
 
-    <Grid container justify='center'
-          className={classes.gridHolder}
-    >
-        <Grid item xs={12} justify='center' className={classes.root}>
-    
-       
-        <a className={classes.a} rel='noopener noreferrer' href={PDF} target='_blank' > <Button className={classes.button} > Open Resume PDF </Button></a>
+      <Grid container justify="center" className={classes.gridHolder}>
+        <Grid item xs={12} justify="center" className={classes.root}>
+          <a
+            className={classes.a}
+            rel="noopener noreferrer"
+            href={PDF}
+            target="_blank"
+          >
+            {" "}
+            <Button className={classes.button}> Open Resume PDF </Button>
+          </a>
 
-          <Grid container justify='center'>
-            <Grid item xs={12} md={4} justify='center' className={classes.techHolder} >
-
-              <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target='_blank' rel='noopener noreferrer' ><DevIcon icon='javascript' style={devIconStyle} /></a>
-              <a href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'> <DevIcon icon='react' style={devIconStyle} /> </a>
-              <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5' target='_blank' rel='noopener noreferrer' ><DevIcon icon='html5' style={devIconStyle} /></a>
-              <a href='https://nodejs.org/en/' target='_blank' rel='noopener noreferrer'> <DevIcon icon='nodejs' style={devIconStyle} /></a>
-              <a href='https://developer.mozilla.org/en-US/docs/Glossary/CSS' target='_blank' rel='noopener noreferrer' ><DevIcon icon='css3' style={devIconStyle} /></a>
-              <a href='https://www.postgresql.org/' target='_blank' rel='noopener noreferrer'><DevIcon icon='postgresql' style={devIconStyle} /></a>
-              <a href='https://www.mysql.com/' target='_blank' rel='noopener noreferrer'><DevIcon icon='mysql' style={devIconStyle} /></a>
-              <a href='https://www.mongodb.com/' target='_blank' rel='noopener noreferrer'><DevIcon icon='mongodb' style={devIconStyle} /></a>
-              <a href='https://git-scm.com/' target='_blank' rel='noopener noreferrer'> <DevIcon icon='git' style={devIconStyle} /></a>
-              
+          <Grid container justify="center">
+            <Grid
+              item
+              xs={12}
+              md={4}
+              justify="center"
+              className={classes.techHolder}
+            >
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="javascript" style={devIconStyle} />
+              </a>
+              <a
+                href="https://reactjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="react" style={devIconStyle} />{" "}
+              </a>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="html5" style={devIconStyle} />
+              </a>
+              <a
+                href="https://nodejs.org/en/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="nodejs" style={devIconStyle} />
+              </a>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Glossary/CSS"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="css3" style={devIconStyle} />
+              </a>
+              <a
+                href="https://www.postgresql.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="postgresql" style={devIconStyle} />
+              </a>
+              <a
+                href="https://www.mysql.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="mysql" style={devIconStyle} />
+              </a>
+              <a
+                href="https://www.mongodb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DevIcon icon="mongodb" style={devIconStyle} />
+              </a>
+              <a
+                href="https://git-scm.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="git" style={devIconStyle} />
+              </a>
+              <a
+                href="https://dotnet.microsoft.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="dotnet" style={devIconStyle} />
+              </a>
+              <a
+                href="https://www.docker.com//"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="docker" style={devIconStyle} />
+              </a>
+              <a
+                href="https://www.npmjs.com//"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="npm" style={devIconStyle} />
+              </a>
+              <a
+                href="https://en.wikipedia.org/wiki/Responsive_web_design/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <DevIcon icon="responsive" style={devIconStyle} />
+              </a>
+            </Grid>
           </Grid>
-       </Grid>
-     
         </Grid>
-     </Grid>
-     </React.Fragment>
+      </Grid>
+    </React.Fragment>
   );
 }
