eeb0b49f206e34f7bcdaae44294204cc4c91b3bc
Michael Stallings <stall84@gmail.com>
2020-09-13 05:54:33 -0400

    Temporarily Removed Contact component from site while under dev. Resized all screenshot images for Portfolio component to speed client load-times. Added media-query component to Resume and resized icon images accordingly. Fix-positioned Footer component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+//import Contact from "./components/Contact";
 
 export default function App() {
-    return (
+  return (
     <React.Fragment>
-       <Route exact path='/' component={Home} />
-       <Route exact path='/resume' component={Resume} />
-       <Route exact path='/portfolio' component={Portfolio} /> 
-       <Route exact path='/contact' component={Contact} />
-    </React.Fragment>       
-    )
+      <Route exact path="/" component={Home} />
+      <Route exact path="/resume" component={Resume} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      {/* <Route exact path='/contact' component={Contact} /> */}
+    </React.Fragment>
+  );
 }
