b34331cc58e8c6b20ea902d60ec48f99e5267b5b
Michael Stallings <stall84@gmail.com>
2022-05-05 22:55:31 -0400

    Adding a Sanity CMS 'backend'

---
+++ mesnet-sanity/schemas/heroContent.js
@@ -0,0 +1,22 @@
+export default {
+  name: "heroContent",
+  type: "document",
+  title: "Hero Content",
+  fields: [
+    {
+      name: "nameBanner",
+      type: "string",
+      title: "Name Banner",
+    },
+    {
+      name: "techStack",
+      type: "string",
+      title: "Tech Stack (List)",
+    },
+    {
+      name: "introParagraph",
+      type: "string",
+      title: "Intro Paragraph",
+    },
+  ],
+};
